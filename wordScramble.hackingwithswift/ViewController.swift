bec89702edd2caa397c0322890b31ec2fb37cebe
Shapovalov_Ilay <greateman@mail.ru>
2022-04-01 01:46:31 +0300

    this time it's all aboult optionaks in UIAlertController

--- wordScramble.hackingwithswift/ViewController.swift
+++ wordScramble.hackingwithswift/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UITableViewController {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 allWords = startWords.components(separatedBy: "\n")
                 print("allWords-array got \(allWords.count) words")
+                
+                navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
             }
         }
         
@@ -40,6 +42,23 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
         cell.textLabel?.text = usedWords[indexPath.row]
         return cell
+    }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+        
     }
 
 }
