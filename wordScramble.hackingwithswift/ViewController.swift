96cf38c9f38c0b0b3d7f3324daf01ffbfb8376a5
Shapovalov_Ilay <greateman@mail.ru>
2022-03-31 18:01:57 +0300

    start project5. Word scramble by hackingwithswift.com

---
+++ wordScramble.hackingwithswift/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  wordScramble.hackingwithswift
+//
+//  Created by Илья Шаповалов on 31.03.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var allWords = [String]()
+    var usedWords = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+                print("allWords-array got \(allWords.count) words")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+      startGame()
+    }
+
+    func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+
+}
+
